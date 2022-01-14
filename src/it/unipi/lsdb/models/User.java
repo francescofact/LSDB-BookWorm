1d6e0ef741cefddf2add435aa96d832cd4b0ca85
SteffenSnippen <97681684+SteffenSnippen@users.noreply.github.com>
2022-01-14 10:53:37 +0100

    Add files via upload

--- src/it/unipi/lsdb/models/User.java
+++ src/it/unipi/lsdb/models/User.java
@@ -1,21 +1,39 @@
-package it.unipi.lsdb.models;
+package it.unipi.giar.Data;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
-public class User {
+import javax.xml.bind.DatatypeConverter;
 
-    private String username;
-    private String password;
-    private Map<Book, Float> ratings;
+import org.neo4j.driver.v1.Record;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
+import org.neo4j.driver.v1.Transaction;
+import org.neo4j.driver.v1.TransactionWork;
+import org.bson.Document;
+import org.bson.conversions.Bson;
 
-    public User(String username, String password){
-        this.username = username;
-        this.password = password;
-        this.ratings =  new HashMap<Book, Float>();
-    }
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoIterable;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
 
-    public String getUsername(){
-        return username;
-    }
-}
+import it.unipi.giar.GiarSession;
+import it.unipi.giar.MongoDriver;
+import it.unipi.giar.Neo4jDriver;
+
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Accumulators.sum;
+import static com.mongodb.client.model.Aggregates.group;
+import static com.mongodb.client.model.Aggregates.match;
+import static com.mongodb.client.model.Aggregates.unwind;
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Aggregates.limit;
+import static com.mongodb.client.model.Aggregates.sort;
+import static com.mongodb.client.model.Sorts.descending;
+
+import static org.neo4j.driver.v1.Values.parameters;

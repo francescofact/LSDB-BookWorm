843379d596c32ce9186e117fdbdca235f1f6f8cd
nuno99freitas <81647593+nuno99freitas@users.noreply.github.com>
2022-02-13 19:22:41 +0000

    Create Rating.java

---
+++ src/it/unipi/lsdb/models/Rating.java
@@ -0,0 +1,14 @@
+
+
+public class Rating {
+    String book;
+    String user;
+    double value;
+    int id;
+    public Rating(String b, String u , double v){
+        this.book=b;
+        this.user=u;
+        this.value=v;
+    }
+
+}

f88981d349c61446ece52b47d6c46d2ef2266ea8
francescofact <ffattori@me.com>
2022-02-15 16:22:06 +0100

    also random rating

--- src/insert_users.py
+++ src/insert_users.py
@@ -4,6 +4,10 @@ import pymongo
 server = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = server["db"]
 mycol = mydb["Users"]
+mybooks = mydb["Books"]
+
+
+print()
 
 
 f = open('users.csv')
@@ -14,5 +18,10 @@ for i in range(1,200):
     username = f.readline().split(",")[0]
     password = f2.readline().split(",")[0]
     print((username, password))
-    user = { "username": username, "password": password, "type": "basic", "Ratings":[] }
+    #random reviews
+    ratings = []
+    for i in range (1,15):
+        book = mybooks.aggregate([{"$sample": {"size": 1}}]).next()
+        ratings.append({"book": book["title"], "value": random.randrange(1,5)})
+    user = { "username": username, "password": password, "type": "basic", "Ratings":ratings }
     x = mycol.insert_one(user)
